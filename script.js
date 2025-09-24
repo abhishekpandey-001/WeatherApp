5f46f9b42fd54a2c845c45473d28f3a4ad3448f9
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2025-09-24 09:41:06 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const cityInput = document.querySelector('.city-input');
+const searchBtn = document.querySelector(".search-btn");
+
+searchBtn.addEventListener('click', ()=>{
+    if(cityInput.value.trim()!=''){
+        console.log(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur();
+    }
+})
+
+
+cityInput.addEventListener('key', (e)=>{
+    if(e.key = 'Enter' && cityInput.value.trim()!=''){
+        
+    }
+})
